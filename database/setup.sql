f9264e08345480a63f24b1aa8fc51ec3f36d71d7
MintoJazz <jinzo.matt@gmail.com>
2026-05-18 16:49:45 -0300

    refat: setup the database by the flask cli

--- database/setup.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS ticket_tracker;
-CREATE DATABASE ticket_tracker;
--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from flask import Flask
 from routes.dashboard import bp as dashboard_bp
 from routes.ranking import bp as ranking_bp
 from routes.tickets import bp as tickets_bp
+from cli import db_push
 
 app = Flask(__name__)
 
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(ranking_bp)
 app.register_blueprint(tickets_bp)
+
+app.cli.add_command(db_push)
